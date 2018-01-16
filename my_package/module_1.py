3279d736abdde3373e1c39270e0d7371b98d1a3c
dannywu0820 <livebetterlife0820@gmail.com>
2018-01-16 15:45:36 +0800

    [feature]: learn more about exception

--- my_package/module_1.py
+++ my_package/module_1.py
@@ -1,3 +1,4 @@
+import sys
 import traceback
 
 variable_1 = "Hello, I'm varaible_1"
@@ -25,14 +26,29 @@ class Module_1:
                 raise ValueError
             elif input_number == 2:
                 raise EOFError
-            else:
+            elif input_number == 3:
                 raise SyntaxError
+            elif input_number == 4:
+                raise RuntimeWarning
+            else:
+                raise SyntaxWarning
         except ValueError:
             print("'exception' block: ValueError")
         except (EOFError, KeyboardInterrupt):
             print("'exception' block: EOFError/KeyboardInterrupt")
-        except:
+        except SyntaxError:
             traceback.print_exc()
+        except RuntimeWarning:
+            exception_type, exception_message, traceback_object = sys.exc_info()
+            while traceback_object:
+                print("[Traceback Object]")
+                print(exception_type)
+                print(exception_message)
+                print('function or module？', traceback_object.tb_frame.f_code.co_name)
+                print('file？', traceback_object.tb_frame.f_code.co_filename)
+                traceback_object = traceback_object.tb_next
+        except SyntaxWarning as e:
+            raise RuntimeWarning from e
         else:
             print("'else' block: execute if no exception happened")
         finally:
