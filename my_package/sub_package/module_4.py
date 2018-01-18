cb23268c9d67ba058af3a5586ba5205105d8d955
dannywu0820 <livebetterlife0820@gmail.com>
2018-01-18 12:49:16 +0800

    [feature]: learn about 'inheritance' and 'abstract class'

--- my_package/sub_package/module_4.py
+++ my_package/sub_package/module_4.py
@@ -1,3 +1,47 @@
 import sys
+from abc import ABCMeta, abstractmethod
 
-print("This is module 4")
+class human(metaclass=ABCMeta):
+    def __init__(self, race):
+        self.race = race
+
+    def selfIntro(self):
+        print("I'm %s" % self.race)
+        
+    @abstractmethod
+    def doSpecificWork(self):
+        pass
+
+class male(human):
+    def __init__(self, race):
+        super(male, self).__init__(race)
+        self.gender = "male"
+        
+    def selfIntro(self):
+        print("I'm %s %s" % (self.race, self.gender))
+        
+    def doSpecificWork(self):
+        print("%s do hunting" % self.gender)
+
+class female(human):
+    def __init__(self, race):
+        super(female, self).__init__(race)
+        self.gender = "female"
+        
+    def selfIntro(self):
+        print("I'm %s %s" % (self.race, self.gender))
+        
+    def doSpecificWork(self):
+        print("%s do collecting" % self.gender)
+    
+class children:
+    def __init__(self):
+        self.age = "children"
+    
+class boy(male, children):
+    def __init__(self, race):
+        male.__init__(self, race)
+        children.__init__(self)
+        
+    def selfIntro(self):
+        print("I'm %s %s %s" % (self.race, self.gender, self.age))
